51e60fd03e49c820270a4d68462291739611d38e
associacaoitag-beep <associacaoitag@gmail.com>
2026-06-24 02:32:43 +0100

    fix: Add missing kDebugMode import from flutter/foundation.dart
    - Import 'package:flutter/foundation.dart' to fix undefined 'kDebugMode' errors
    - Resolves 5 compilation errors on lines 84, 88, 91, 104, and 116

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 import '../providers/channel_provider.dart';
 import '../utils/app_colors.dart';
